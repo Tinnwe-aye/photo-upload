6f0368000c8056a45f53e1f672add5bff31b8677
TinNweAye <tinnweaye@brycenmyanamr.com.mm>
2023-10-20 00:14:11 +0630

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,22 +37,22 @@
 
 <nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
     <div class="container">
-    {{-- <a class="navbar-brand" href="#">Laravel</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    {{-- <a class="navbar-brand" href="#">Laravel</a> --}}
+    {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button> --}}
 
-    {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link" href="#">Login</a>
-            </li>
+            </li> --}}
             <li class="nav-item">
-                <a class="nav-link" href="#">Register</a>
+                <a class="nav-link" href="/list">Photo List</a>
             </li>
         </ul>
 
-    </div> --}}
+    </div>
     </div>
 </nav>
 <main class="my-form">
